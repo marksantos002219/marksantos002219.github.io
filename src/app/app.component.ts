4974cb65c78e164d57ab9e16bf390d218a1b97b9
mark santos <marksantos002219@gmail.com>
2022-07-13 17:45:23 +0800

    sdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Html5Qrcode } from 'html5-qrcode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scanner';
+
+  async ngOnInit(): Promise<void> {
+    const cameras = await Html5Qrcode.getCameras();
+    if (!cameras.length) {
+      return alert('Camera is required.')
+    }
+    const scanner = new Html5Qrcode('scanner');
+    const success = (decodedText: any, decodedResult: any) => {
+      console.log(decodedText, decodedResult);
+    }
+    const error = (err: any) => {
+      console.log(err);
+    }
+    scanner.start(cameras[0].id, {
+      fps: 10,
+      qrbox: {
+        width: 250,
+        height: 250
+      }
+    }, success, error);
+  }
 }
