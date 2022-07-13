73e188772ce61e561ec4f715a9e08163d0cc9aac
mark santos <marksantos002219@gmail.com>
2022-07-13 18:01:31 +0800

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Html5Qrcode } from 'html5-qrcode';
+import { Html5Qrcode, Html5QrcodeScanner, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,10 @@ export class AppComponent implements OnInit {
 
   title = 'scanner';
 
-  async ngOnInit(): Promise<void> {
+  ngOnInit(): void {
+  }
+
+  async scanFirst(): Promise<void> {
     const cameras = await Html5Qrcode.getCameras();
     if (!cameras.length) {
       return alert('Camera is required.')
@@ -29,5 +32,49 @@ export class AppComponent implements OnInit {
         height: 250
       }
     }, success, error);
+  }
+
+  scanSecond(): void {
+    const formatsToSupport: Html5QrcodeSupportedFormats[] = [
+      Html5QrcodeSupportedFormats.QR_CODE,
+      Html5QrcodeSupportedFormats.AZTEC,
+      Html5QrcodeSupportedFormats.CODABAR,
+      Html5QrcodeSupportedFormats.CODE_39,
+      Html5QrcodeSupportedFormats.CODE_93,
+      Html5QrcodeSupportedFormats.CODE_128,
+      Html5QrcodeSupportedFormats.DATA_MATRIX,
+      Html5QrcodeSupportedFormats.MAXICODE,
+      Html5QrcodeSupportedFormats.ITF,
+      Html5QrcodeSupportedFormats.EAN_13,
+      Html5QrcodeSupportedFormats.EAN_8,
+      Html5QrcodeSupportedFormats.PDF_417,
+      Html5QrcodeSupportedFormats.RSS_14,
+      Html5QrcodeSupportedFormats.RSS_EXPANDED,
+      Html5QrcodeSupportedFormats.UPC_A,
+      Html5QrcodeSupportedFormats.UPC_E,
+      Html5QrcodeSupportedFormats.UPC_EAN_EXTENSION,
+    ];
+    const scanner = new Html5QrcodeScanner(
+      'scanner',
+      {
+        fps: 10,
+        qrbox: {
+          height: 250,
+          width: 250,
+        },
+        formatsToSupport: formatsToSupport,
+        supportedScanTypes: [],
+        disableFlip: false,
+        videoConstraints: { facingMode: 'environment' },
+      },
+      false,
+    );
+    const success = (result: any) => {
+      console.log(result);
+    };
+    const error = (err: any) => {
+      console.log(err);
+    };
+    scanner.render(success, error);
   }
 }
